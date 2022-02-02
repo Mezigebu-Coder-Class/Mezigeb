ff11fdee332db68eac2121f7bfd31d1b9e0d9c6d
wku-cslab <mezigebu1>
2022-02-01 20:01:58 -0800

    my first commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module com.example.mezigeb {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens com.example.mezigeb to javafx.fxml;
+    exports com.example.mezigeb;
+}
